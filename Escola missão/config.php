be822277c53b71950913a0ead02eefb4c8457ad5
Miguel <potestatem.contasmkt@gmail.com>
2026-08-25 12:43:50 -0300

    Delete Escola missão directory

--- Escola missão/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'google_api_key' => getenv('GOOGLE_API_KEY') ?: 'AIzaSyCz-EWKpGRwDaZl9r4bxn0ufS0C8lfXSB8',
-    'google_model' => getenv('GOOGLE_MODEL') ?: 'gemini-2.5-flash',
-    'elevenlabs_api_key' => getenv('ELEVENLABS_API_KEY') ?: 'sk_d1262295c28cf6afe4674967145d63c3ee1cd6ea2c975733',
-    'elevenlabs_voice_id' => getenv('ELEVENLABS_VOICE_ID') ?: 'bIHbv24MWmeRgasZH58o',
-    'elevenlabs_model_id' => getenv('ELEVENLABS_MODEL_ID') ?: 'eleven_multilingual_v2',
-];
--- Escola missão/iniciar-servidor.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-start "Servidor Escola Missao" powershell -NoExit -ExecutionPolicy Bypass -File "%~dp0server.ps1"
