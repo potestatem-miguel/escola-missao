156c8a5d9b668fa0723fd7cb3170edeec66ebb88
Miguel <potestatem.contasmkt@gmail.com>
2026-08-25 12:40:59 -0300

    Delete Escola missão directory

--- Escola missão/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'google_api_key' => 'AIzaSyCz-EWKpGRwDaZl9r4bxn0ufS0C8lfXSB8',
-    'google_model' => 'gemini-2.5-flash',
-    'elevenlabs_api_key' => 'sk_d1262295c28cf6afe4674967145d63c3ee1cd6ea2c975733',
-    'elevenlabs_voice_id' => 'bIHbv24MWmeRgasZH58o',
-    'elevenlabs_model_id' => 'eleven_multilingual_v2',
-];
--- Escola missão/supabase-config.js
+++
@@ -1,4 +0,0 @@
-window.ESTUDA_SUPABASE_CONFIG = {
-  url: "https://ccdtpcjxzdrjsafzhzgn.supabase.co",
-  anonKey: "sb_publishable_oVT3Nb5eAGrDqVjN8NoAqA_1YifK4Rw"
-};
